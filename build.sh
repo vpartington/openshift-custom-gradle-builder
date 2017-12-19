7bfe1b9b606ecf81405b5491baa760691db5cd8a
Vincent Partington <vpartington@xebialabs.com>
2017-12-19 15:36:29 +0100

    Fixed custom builder to work with the build.gradle file from vpartington/rest-o-rant-api.

--- build.sh
+++ build.sh
@@ -25,29 +25,26 @@ if [[ "${SOURCE_REPOSITORY}" != "git://"* ]] && [[ "${SOURCE_REPOSITORY}" != "gi
   fi
 fi
 
-if [ -n "${SOURCE_REF}" ]; then
-  BUILD_DIR=$(mktemp --directory)
-  git clone --recursive "${SOURCE_REPOSITORY}" "${BUILD_DIR}"
-  if [ $? != 0 ]; then
-    echo "Error trying to fetch git source: ${SOURCE_REPOSITORY}"
-    exit 1
-  fi
-  pushd "${BUILD_DIR}"
-  git checkout "${SOURCE_REF}"
-  if [ $? != 0 ]; then
-    echo "Error trying to checkout branch: ${SOURCE_REF}"
-    exit 1
-  fi
-  popd
-  docker build --rm -t "${TAG}" "${BUILD_DIR}"
-else
-  docker build --rm -t "${TAG}" "${SOURCE_REPOSITORY}"
+BUILD_DIR=$(mktemp --directory)
+git clone --recursive "${SOURCE_REPOSITORY}" "${BUILD_DIR}"
+if [ $? != 0 ]; then
+  echo "Error trying to fetch git source: ${SOURCE_REPOSITORY}"
+  exit 1
 fi
+pushd "${BUILD_DIR}"
+git checkout "${SOURCE_REF}"
+if [ $? != 0 ]; then
+  echo "Error trying to checkout branch: ${SOURCE_REF}"
+  exit 1
+fi
+
+./gradlew pushDockerImage -Doutput.registry=${OUTPUT_REGISTRY} -Doutput.image=${OUTPUT_IMAGE}
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+popd
 
 if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
   cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
-fi
-
-if [ -n "${OUTPUT_IMAGE}" ] || [ -s "/root/.dockercfg" ]; then
-  docker push "${TAG}"
 fi
